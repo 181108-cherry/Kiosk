ff535ceb69cbb737888b1ac313de18921bc023c6
강다연 <dysy9799@naver.com>
2025-03-09 19:25:50 +0900

    실행시 메뉴판 생성

--- src/level1/Main.java
+++ src/level1/Main.java
@@ -1,4 +1,15 @@
 package level1;
 
 public class Main {
+    public static void main(String[] args) {
+
+        System.out.println(" ____________________________________________________________");
+        System.out.println(" 1. Espresso      ㅣ💰 2.5 ㅣ 커피의 기본");
+        System.out.println(" 2. Americano     ㅣ💰 3.5 ㅣ 심플함 속 깊은 풍미");
+        System.out.println(" 3. Caffe Latte   ㅣ💰 4.5 ㅣ 커피와 고소한 우유");
+        System.out.println(" 4. Vanilla Latte ㅣ💰 5.0 ㅣ 천연 바닐라와 고소한 우유 ");
+        System.out.println(" 5. Cream Mocha   ㅣ💰 5.5 ㅣ 진한 라떼에 수제 크림이 올라간 커피 ");
+        System.out.println(" 6. Einspänner    ㅣ💰 5.5 ㅣ 블랙커피 위에 수제 크림이 올라간 커피 ");
+        System.out.println(" ____________________________________________________________");
+    }
 }
